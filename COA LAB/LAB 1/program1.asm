698766211b4f5a3e968c380123e030e244382fa2
Avinash Changrani <74554800+nabobery@users.noreply.github.com>
2022-02-27 15:29:06 +0000

    Added Program1

--- COA LAB/LAB 1/program1.asm
+++ COA LAB/LAB 1/program1.asm
@@ -4,20 +4,20 @@ global _start
 
 _start:
     mov eax, [num1] ; move first num to ecx
- 	cmp eax, [num2] ; compare num1 and num2
+    cmp eax, [num2] ; compare num1 and num2
     jg check_third  ; if num1 > num2
     mov eax, [num2] ; else move num2 to ecx
     
     check_third:
-        cmp eax, [num3] ; compare ecx and num3
-        jg check_fourth ; if num in ecx > num3
-        mov eax, [num3] ; else move num3 to ecx
+	cmp eax, [num3] ; compare ecx and num3
+	jg check_fourth ; if num in ecx > num3
+	mov eax, [num3] ; else move num3 to ecx
     
     check_fourth:
-        cmp eax, [num4] ; compare ecx and num4
-        jg check_fifth  ; if num in ecx > num4
-        mov eax, [num4] ; else move num4 to ecx
-    
+	cmp eax, [num4] ; compare ecx and num4
+	jg check_fifth  ; if num in ecx > num4
+	mov eax, [num4] ; else move num4 to ecx
+
     check_fifth: 
         cmp eax, [num5] ; compare ecx and num4
         jg _exit        ; if num in ecx > num5
@@ -30,13 +30,13 @@ _exit:
     sub edi, largest                ; EDI (pointer to the terminating NULL) - pointer to sum = length of the string
     mov [largest_len], edi
     mov eax,4          ; The system call for write (sys_write)
-	mov ebx,1          ; File descriptor 1 - standard output
-	mov ecx,msg        ; move offset of msg to ecx
- 	mov edx, len       ; move length of msg to edx
- 	int 0x80           ; call kernel
+    mov ebx,1          ; File descriptor 1 - standard output
+    mov ecx,msg        ; move offset of msg to ecx
+    mov edx, len       ; move length of msg to edx
+    int 0x80           ; call kernel
  	
     mov eax,4               ; The system call for write (sys_write)
-	mov ebx,1               ; File descriptor 1 - standard output
+    mov ebx,1               ; File descriptor 1 - standard output
     mov ecx, largest        ; move offset of largest to ecx
     mov edx, [largest_len]  ; move length of largest to edx
     int 0x80                ; call kernel
@@ -55,7 +55,7 @@ int2str:    ; Converts an positive integer in EAX to a string pointed to by EDI
     inc ecx                 ; Increment push counter
     test eax, eax           ; Anything left to divide?
     jnz .LL1                ; Yes: loop once more
-    .LL2:                       ; Second loop: Retrieve the remainders
+    .LL2:                   ; Second loop: Retrieve the remainders
     pop dx                  ; In DL is the value
     or dl, '0'              ; To ASCII
     mov [edi], dl           ; Save it to the string
